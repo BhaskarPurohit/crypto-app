42356eb39bf80bccee6b43626a2df0c532b817b6
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-27 14:44:55 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import CoinDetails from "./components/CoinDetails"
+import Coins from "./components/Coins";
+import Home  from "./components/Home";
 import React from "react";
+import Exchanges from "./components/Exchanges";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/coins" element={<Coins/>}/>
+        <Route path="/coindetails" element={<CoinDetails/>}/>
+        <Route path="/exchanges" element={<Exchanges/>}/>
+      </Routes>
+    </Router>
     
     
   );
