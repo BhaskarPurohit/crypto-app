3e425128fb8a4313bd56486937a3d21809d69a05
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-27 15:25:37 +0530

    new

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {HStack,Button,Link} from "@chakra-ui/react"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <HStack p={'4'} shadow={'base'} bgColor={'blackAlpha.900'}>
+        <Button>
+            <Link></Link>
+        </Button>
+
+    </HStack>
   )
 }
 
