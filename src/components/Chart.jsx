92d3bcd00d2cb8a65b1f8af08db05b0d66ee179e
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-06-29 23:06:56 +0530

    new

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {} from "react-chartjs-2"
+import {Line} from "react-chartjs-2"
 import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from "chart.js"
+import { background  } from '@chakra-ui/react'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
@@ -17,7 +18,15 @@ const Chart = ({arr=[], currency, days}) => {
 
 
   return (
-    <Line options={{responsive:true,}} data={data}/>
+    <Line options={{responsive:true,}} data={{
+        labels: date,
+        datasets:[{
+            label:`Price in ${currency}`,
+            data: prices, borderColor:"rgb(255,99,132)",
+            backgroundColor: "rgba(255,99,132,0.5)"
+        }]
+    }
+    }/>
   )
 }
 
